084610830b7369ccdad814dfbe0221cb63fc1ab1
Virgile Gerecke <virgile.gerecke@unistra.fr>
2020-02-13 14:26:02 +0100

    Added detection of deletions (with an heavy cache)

--- src/fr/unistra/dnum/ade/AdeClient.java
+++ src/fr/unistra/dnum/ade/AdeClient.java
@@ -8,12 +8,13 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class AdeClient {
     private JSONObject adeConf;
@@ -41,29 +42,84 @@ public class AdeClient {
         UUID uuid = UUID.randomUUID();
         doc.put("operation_id", uuid.toString());
 
+        // Recent changes
         FiltersEvents fe = new FiltersEvents();
         fe.addFilterUpdatedStart(lastRun);
         List<Element> events = api.getEvents(fe, 8).getChildren();
         int nb_events = events.size();
-        for (Element event : events) {
-            JSONObject j_event = new JSONObject();
-            j_event.put("id", event.getString("id"));
-            for (Object c : event.getContent()) {
-                Element content = (Element) c;
-                if (content.getName().equals("resources")) {
-                    for (Object r : content.getContent()) {
-                        Element resource = (Element) r;
-                        String category = ((Element) r).getString("category");
-                        // ONly selected categories
-                        if (categories.contains(category))
-                            j_event.append("resources", resource.getInt("id"));
-                    }
-                }
-            }
-            doc.append("events", j_event);
-        }
         logger.info("Found " + nb_events + " updated events");
+        if(nb_events > 0){
+            doc.put("events", eventsToJson(events, "update").getJSONArray("events"));
+        }
+
+        // Hunt for missing events
+        fe = new FiltersEvents();
+        events = api.getEvents(fe, 8).getChildren();
+        JSONObject old_ones = read_cache();
+        HashMap<String, Element> eventsMap = new HashMap<String, Element>();
+        for (Element event : events) {
+            eventsMap.put(event.getString("id"), event);
+        }
+        for (Object o : old_ones.getJSONArray("events")) {
+            JSONObject obj = (JSONObject) o;
+            if (eventsMap.get(obj.getString("id")) == null) {
+                nb_events++;
+                logger.info("Deleted event " + obj.getString("id"));
+                obj.put("operation", "deletion");
+                doc.append("events", obj);
+            }
+        }
+        write_cache(eventsToJson(events, null));
+
+        logger.info("Found " + nb_events + " updated or deleted events");
         if (nb_events == 0) return null;
         return doc;
     }
+
+    private void write_cache(JSONObject data) throws IOException {
+        String filePath = adeConf.getString("cache_file");
+        FileOutputStream fos = new FileOutputStream(new File(filePath));
+        fos.write(data.toString().getBytes());
+        fos.close();
+    }
+
+    private JSONObject read_cache() throws IOException {
+        String filePath = adeConf.getString("cache_file");
+        File f = new File(filePath);
+        if ((!f.exists()) || (!f.canRead())) {
+            return new JSONObject("{\"events\": []}");
+        } else {
+            return new JSONObject(new String(Files.readAllBytes(Paths.get(filePath))));
+        }
+    }
+
+    private JSONObject eventsToJson(List<Element> events, String operation) {
+        JSONObject doc = new JSONObject();
+        for (Element event : events) {
+            JSONObject j_event = eventToJson(event);
+            if(operation != null) j_event.put("operation", operation);
+            doc.append("events", j_event);
+        }
+        return doc;
+    }
+
+    private JSONObject eventToJson(Element event) {
+        JSONObject j_event = new JSONObject();
+        j_event.put("id", event.getString("id"));
+        for (Object c : event.getContent()) {
+            Element content = (Element) c;
+            if (content.getName().equals("resources")) {
+                for (Object r : content.getContent()) {
+                    Element resource = (Element) r;
+                    String category = ((Element) r).getString("category");
+                    // ONly selected categories
+                    if (categories.contains(category))
+                        j_event.append("resources", resource.getInt("id"));
+                }
+            }
+        }
+        return j_event;
+    }
+
+
 }
